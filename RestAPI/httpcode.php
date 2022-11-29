4beb30336688ca86365bc5c68332b9668c7ab2eb
J-hoplin1 <jhoplin7259@gmail.com>
2022-11-29 22:50:04 +0900

    feat : add middleware function

--- RestAPI/httpcode.php
+++ RestAPI/httpcode.php
@@ -1,7 +1,20 @@
 <?php
+
+include_once "codes.php";
+
 class HTTPcodes{
     /**
-     * all of common http status code should be like this
+     * available HTTP METHOD
+     *
+     */
+    protected $validhttpmethod = array(
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE"
+    );
+    /**
+     * All common http status code should be like this
      *
      * {
      * "code"
@@ -10,19 +23,33 @@ class HTTPcodes{
      */
     protected static $OK = array(
         "code" => 200,
-        "msg" => "OK"
+        "msg" => OK
     );
     protected static $INVALIDREQUEST = array(
         "code" => 400,
-        "msg" => "Bad request"
+        "msg" => ERR_400
     );
     protected static $INVALIDHTTPMETHOD = array(
         "code" => 405,
-        "msg" => "Method not allowed"
+        "msg" => ERR_405
     );
     protected static $INVALIDRETURNTYPE = array(
         "code" => 500,
-        "msg" => "Invalid return type. Type should be jsonstring or string"
+        "msg" => ERR_500
+    );
+    protected static $MIDDLEWARENOTCALLABLE = array(
+        "code" => 500,
+        "msg" => ERR_MIDDLEWARE_NOT_CALLABLE
+    );
+
+    protected static $MIDDLEWARESHOULDBEARRAY = array(
+        "code" => 500,
+        "msg" => ERR_MIDDLEWARE_NOT_ARRAY
+    );
+
+    protected static $MIDDLEWREEXCEPTIONOCCURED = array(
+        "code" => 500,
+        "msg" =>ERR_MIDDLEWARE_EXCEPTION
     );
 }
 
